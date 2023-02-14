4f0141c22f6e13d6b3e6eb15bb32a9e604c15000
djc06048 <gsafe1213@gmail.com>
2023-02-14 19:06:14 +0900

    test: post 관련 테스트 코드 수정

--- src/test/java/com/example/toyproject/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/example/toyproject/domain/posts/PostsRepositoryTest.java
@@ -4,43 +4,37 @@ package com.example.toyproject.domain.posts;
 
 import com.example.toyproject.domain.user.User;
 import com.example.toyproject.domain.user.UserRepository;
+import com.example.toyproject.web.utils.WrongUserExceptions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PostsRepositoryTest {
     @Autowired
     PostsRepository postsRepository;
     @Autowired
-    UserRepository usersRepository;
+    UserRepository userRepository;
 
-
-
-//    @After
-//    public void cleanup(){
-//        usersRepository.deleteAll();
-//        postsRepository.deleteAll();
-//    }
 
     @Test
     public void post_save() {
         //given
-        String name = "cheol";
-        String pw = "123";
-        User user = usersRepository.save(User.builder().password(pw).name(name).build());
+        String user_email = "gsafe1213@gmail.com";
+        User user = userRepository.findByEmail(user_email).orElseThrow(() -> new WrongUserExceptions("해당 하는 유저가 존재하지 않습니다"));
 
-
-
-        String title = "title";
-        String content = "content";
-        Posts post = postsRepository.save(Posts.builder().title(title).content(content).writer(user.getEmail()).build());
+        String title = "테스트용 글 제목";
+        String content = "테스트용 글 내용";
+        postsRepository.save(Posts.builder().title(title).content(content).writer(user.getEmail()).user(user).build());
 
         //when
         List<Posts> postsList = postsRepository.findByTitle(title);
@@ -48,31 +42,10 @@ public class PostsRepositoryTest {
 
         //then
 
+        assertThat(postsList.get(postsList.size() - 1).getTitle()).isEqualTo(title);
+        assertThat(postsList.get(postsList.size() - 1).getContent()).isEqualTo(content);
 
-        assertThat(postsList.get(postsList.size()-1).getTitle()).isEqualTo(title);
-        assertThat(postsList.get(postsList.size()-1).getContent()).isEqualTo(content);
-        assertThat(postsList.get(postsList.size()-1).getUser().getName()).isEqualTo(name);
     }
-//    @Test
-//
-//    public void BaseTimEntity_save(){
-//        //given
-//        LocalDateTime now=LocalDateTime.of(2023,1,12,0,0,0);
-//        postsRepository.save(Posts.builder().content("content").title("title").author(Users.builder().email("@@").password("123").username("hye").build()).build());
-//        //when
-//        List<Posts> postsList=postsRepository.findAll();
-//        //then
-//        Posts posts=postsList.get(0);
-//        System.out.println("posts.getCreateAt() = " + posts.getCreateAt());
-//        System.out.println("posts.getLastModified() = " + posts.getLastModified());
-//
-//        assertThat(posts.getCreateAt()).isAfter(now);
-//        assertThat(posts.getLastModified()).isAfter(now);
-//    }
-
-
-
-
 
 
 }
--- src/test/java/com/example/toyproject/web/controller/posts/PostApiControllerTest.java
+++ src/test/java/com/example/toyproject/web/controller/posts/PostApiControllerTest.java
@@ -1,27 +1,42 @@
 package com.example.toyproject.web.controller.posts;
 
+import com.example.toyproject.domain.posts.Posts;
 import com.example.toyproject.domain.posts.PostsRepository;
 import com.example.toyproject.domain.user.UserRepository;
 import com.example.toyproject.web.dto.posts.PostsResponseDto;
 import com.example.toyproject.web.dto.posts.PostsSaveRequestDto;
 import com.example.toyproject.web.dto.posts.PostsUpdateRequestDto;
+import com.example.toyproject.web.utils.WrongPostsExceptions;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 import java.net.URI;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
@@ -39,108 +54,106 @@ public class PostApiControllerTest {
 
     @Autowired
     private UserRepository usersRepository;
-//    @After
-//    public void tearDown() throws Exception{
-//        postsRepository.deleteAll();
-//        usersRepository.deleteAll();
-//    }
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+    //MockMvc: 실제 객체와 비슷하지만 테스트에 필요한 기능만 가지는 가짜
+    //객체를 만들어 애플리케이션 서버에 배포하지 않고도 스프링 MVC 동작을
+    //재현할 수 있는 클래스
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity()) //스프링 시큐리티를 Spring MVC 테스트와 통합할 때 필요한 모든 초기 세팅을 수행
+                .build();
+    }
 
     @Test
+    @WithMockUser(roles = "USER")
     @Transactional
-    public void save() throws Exception{
+    public void save() throws Exception {
         //given
-        String title="title4";
-        String content="content4";
-        String userId="3";
-        PostsSaveRequestDto requestDto= PostsSaveRequestDto.builder()
+        String title = "테스트용 글제목";
+        String content = "테스트용 글 내용";
+        Long userId = 1L;
+
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
                 .title(title)
                 .content(content)
                 .userId(userId)
                 .build();
 
-        URI uri= UriComponentsBuilder
-                .fromUriString("http://localhost:"+port)
-                        .path("/api/v1/posts").encode()
-                        .build().toUri();
-        System.out.println("uri = " + uri);
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
         //when
-        //HttpHeaders headers = new HttpHeaders();
-        //headers.setContentType(MediaType.APPLICATION_JSON);
-        //headers.add("ContentType", "application/json");
-        //HttpEntity<PostsSaveRequestDto> request=new HttpEntity<>(dto,headers);
         System.out.println(" ###INSERT START");
-        ResponseEntity<PostsResponseDto> responseEntity=restTemplate.postForEntity(uri,requestDto,PostsResponseDto.class);
+
+        ResultActions actions = mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(requestDto)));
+
         System.out.println(" ###INSERT END");
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        System.out.println("responseEntity.toString() = " + responseEntity.toString());
-        if(responseEntity.getBody().getSuccess().equals(true)){
-            assertThat(responseEntity.getBody().getUserId()).isEqualTo(Long.parseLong(userId));
-            assertThat(responseEntity.getBody().getMessage()).isEqualTo("성공적으로 등록되었습니다");
-
-        }else{
-            assertThat(responseEntity.getBody().getMessage()).isEqualTo("해당하는 유저가 존재하지 않습니다");
-
-        }
+        actions.andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
 
 
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     @Transactional
-    public void update() throws Exception{
+    public void update() throws Exception {
         //given
-        String title="updateTitle";
-        String content="updateContent";
-        PostsUpdateRequestDto requestDto= PostsUpdateRequestDto.builder()
+        Long postId = 1L;
+        Long userId = 1L;
+        String title = "테스트용 글 업데이트 제목";
+        String content = "테스트용 글 업데이트 내용";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
                 .title(title)
                 .content(content)
                 .build();
-        Long userId=1L;
-        Long postId=1L;
-        URI uri= UriComponentsBuilder
-                .fromUriString("http://localhost:"+port)
-                .path("/api/v1/posts/{postId}/users/{userId}").encode()
-                .build().expand(postId,userId).toUri();
-        System.out.println("uri = " + uri);
+
+
+        String url = "http://localhost:" + port + "/api/v1/posts";
+
         //when
-        //HttpHeaders headers = new HttpHeaders();
-        //headers.setContentType(MediaType.APPLICATION_JSON);
-        //headers.add("ContentType", "application/json");
-        //HttpEntity<PostsSaveRequestDto> request=new HttpEntity<>(dto,headers);
         System.out.println(" ###UPDATE START");
-        HttpEntity<PostsUpdateRequestDto> requestEntity=new HttpEntity<>(requestDto);
-        ResponseEntity<PostsResponseDto> responseEntity=restTemplate.exchange(uri,HttpMethod.PUT,requestEntity,PostsResponseDto.class);
+
+        ResultActions actions = mvc.perform(put(url + "/{postId}/users/{userId}", postId, userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(requestDto)));
+
         System.out.println(" ###UPDATE END");
 
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        System.out.println("responseEntity.toString() = " + responseEntity.toString());
-        if(responseEntity.getBody().getSuccess().equals(true)){
-            assertThat(responseEntity.getBody().getUserId()).isEqualTo(userId);
-            assertThat(responseEntity.getBody().getMessage()).isEqualTo("성공적으로 수정되었습니다");
+        actions.andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.content").value(content))
+                .andDo(MockMvcResultHandlers.print());
 
-        }
-        //오류나는 경우 테스트?
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
     }
 
 
     @Test
+    @WithMockUser(roles = "USER")
     @Transactional
-    public void findById() throws Exception{
-        Long postId=5L;
-        String url="http://localhost:"+port+"/api/v1/posts/"+postId;
-        ResponseEntity<PostsResponseDto> responseEntity=restTemplate.getForEntity(url, PostsResponseDto.class);
-        System.out.println(responseEntity.getBody().toString());
+    public void findByPostId() throws Exception {
+        Long postId = 5L;
+        String url = "http://localhost:" + port + "/api/v1/posts";
 
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        if(responseEntity.getBody().getSuccess().equals(true)){
-            assertThat(responseEntity.getBody().getPostId()).isEqualTo(postId);
-            assertThat(responseEntity.getBody().getMessage()).isEqualTo("해당하는 게시물을 불러왔습니다");
+        mvc.perform(get(url + "/{postId}", postId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
 
-        }else{
-            assertThat(responseEntity.getBody().getMessage()).isEqualTo("해당 게시글이 없습니다. id="+postId);
-        }
+
     }
 }
