8bf4f3b2348abab8fcc0d8e730092a467a0ab5c3
djc06048 <gsafe1213@gmail.com>
2023-02-14 19:03:53 +0900

    test: comment관련 테스트 코드 수정

--- src/test/java/com/example/toyproject/domain/comments/CommentsRepositoryTest.java
+++ src/test/java/com/example/toyproject/domain/comments/CommentsRepositoryTest.java
@@ -1,69 +1,56 @@
-//package com.example.toyproject.domain.comments;
-//
-//
-//import com.example.toyproject.domain.posts.Posts;
-//import com.example.toyproject.domain.posts.PostsRepository;
-//import com.example.toyproject.domain.users.Users;
-//import com.example.toyproject.domain.users.UsersRepository;
-//import org.junit.After;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.List;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//@Transactional
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//public class CommentsRepositoryTest {
-//    @Autowired
-//    PostsRepository postsRepository;
-//    @Autowired
-//    UsersRepository usersRepository;
-//    @Autowired
-//    CommentsRepository commentsRepository;
-//
-//
-////    @After
-////    public void cleanup(){
-////
-////        usersRepository.deleteAll();
-////        commentsRepository.deleteAll();
-////        postsRepository.deleteAll();
-////    }
-//    @Test
-//    public void comment_save() {
-//
-//        //given
-//        String name = "cheol";
-//        String pw = "123";
-//        Users user = usersRepository.save(Users.builder().password(pw).name(name).build());
-//
-//
-//
-//        String title = "title";
-//        String content = "content";
-//        Posts post = postsRepository.save(Posts.builder().title(title).content(content).writer(user.getEmail()).build());
-//
-//        String comment_name = "park";
-//        String comment_pw = "1234";
-//        Users comment_user= usersRepository.save(Users.builder().password(comment_pw).name(comment_name).build());
-//
-//        String commentContent = "축하드려요~";
-//        commentsRepository.save(Comments.builder().content(commentContent).user(comment_user).post(post).build());
-//
-//        //when
-//
-//        //해당 포스트 pk으로 등록된 모든 comment 조회
-//        List<Comments> commentsList = commentsRepository.findByPost_PostId(1L);
-//
-//        //then
-//
-//        assertThat(commentsList.get(0).getUser().getName()).isEqualTo("park");
-//    }
-//}
+package com.example.toyproject.domain.comments;
+
+
+import com.example.toyproject.domain.posts.Posts;
+import com.example.toyproject.domain.posts.PostsRepository;
+import com.example.toyproject.domain.user.User;
+import com.example.toyproject.domain.user.UserRepository;
+import com.example.toyproject.web.utils.WrongPostsExceptions;
+import com.example.toyproject.web.utils.WrongUserExceptions;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommentsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    CommentsRepository commentsRepository;
+
+    @Test
+    public void comment_save() {
+
+        //given
+        String user_email = "gsafe1213@gmail.com";
+        User user = userRepository.findByEmail(user_email).orElseThrow(() -> new WrongUserExceptions("해당 하는 유저가 존재하지 않습니다"));
+
+        Long post_id = 1L;
+        Posts post = postsRepository.findByPostId(post_id).orElseThrow(() -> new WrongPostsExceptions("해당 하는 포스트가 존재하지 않습니다"));
+
+
+        String commentContent = "테스트용 댓글";
+        commentsRepository.save(Comments.builder().writer(user_email).content(commentContent).user(user).post(post).build());
+
+        //when
+
+        //해당 포스트 pk으로 등록된 모든 comment 조회
+        List<Comments> commentsList = commentsRepository.findByPost_PostId(1L);
+
+        //then
+
+        assertThat(commentsList.get(0).getUser().getEmail()).isEqualTo(user_email);
+    }
+}
--- src/test/java/com/example/toyproject/web/controller/comments/CommentApiControllerTest.java
+++ src/test/java/com/example/toyproject/web/controller/comments/CommentApiControllerTest.java
@@ -1,27 +1,44 @@
 package com.example.toyproject.web.controller.comments;
 
+
+import com.example.toyproject.domain.comments.Comments;
+import com.example.toyproject.domain.comments.CommentsRepository;
 import com.example.toyproject.domain.posts.PostsRepository;
+
 import com.example.toyproject.domain.user.UserRepository;
-import com.example.toyproject.web.dto.comments.CommentResponseDto;
+import com.example.toyproject.service.comments.CommentService;
+
 import com.example.toyproject.web.dto.comments.CommentSaveRequestDto;
 import com.example.toyproject.web.dto.comments.CommentUpdateRequestDto;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.security.test.context.support.WithMockUser;
 
-import java.net.URI;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.context.WebApplicationContext;
+
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -33,68 +50,95 @@ public class CommentApiControllerTest {
     private TestRestTemplate restTemplate;
 
     @Autowired
-    private PostsRepository postsRepository;
+    private CommentsRepository commentsRepository;
 
     @Autowired
     private UserRepository usersRepository;
+
+    @Autowired
+    private CommentService commentService;
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+    //MockMvc: 실제 객체와 비슷하지만 테스트에 필요한 기능만 가지는 가짜
+    //객체를 만들어 애플리케이션 서버에 배포하지 않고도 스프링 MVC 동작을
+    //재현할 수 있는 클래스
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity()) //스프링 시큐리티를 Spring MVC 테스트와 통합할 때 필요한 모든 초기 세팅을 수행
+                .build();
+    }
+
     @Test
+    @WithMockUser(roles = "USER")
     @Transactional
-    public void saveComment() throws Exception{
+    public void saveComment() throws Exception {
         //given
-        String content="comment content";
-        Long postId=1L;
-        Long userId=3L;
-        CommentSaveRequestDto requestDto=CommentSaveRequestDto
+        String content = "api 테스트용 댓글";
+        Long postId = 1L;
+        Long userId = 1L;
+
+
+        CommentSaveRequestDto requestDto = CommentSaveRequestDto
                 .builder()
                 .content(content)
                 .postId(postId)
                 .userId(userId)
                 .build();
-        URI uri= UriComponentsBuilder
-                .fromUriString("http://localhost:"+port)
-                .path("/api/v1/comments").encode()
-                .build().toUri();
-        System.out.println("uri = " + uri);
+
+        String url = "http://localhost:" + port + "/api/v1/comments";
+
         //when
         System.out.println(" ###INSERT START");
-        ResponseEntity<CommentResponseDto> responseEntity = restTemplate.postForEntity(uri, requestDto, CommentResponseDto.class);
+        ResultActions actions = mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(requestDto)));
+
         System.out.println(" ###INSERT END");
-        System.out.println("responseEntity.toString() = " + responseEntity.toString());
+
         //then
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        if(responseEntity.getStatusCode().equals(true)){
-            assertThat(responseEntity.getBody().getUserId()).isEqualTo(userId);
-            assertThat(responseEntity.getBody().getPostId()).isEqualTo(postId);
-        }
+        actions.andExpect(status().isOk())
+                .andDo(MockMvcResultHandlers.print());
 
 
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     @Transactional
-    public void updateComment() throws Exception{
-        String content="updateComment!!!";
-        Long postId=1L;
-        Long userId=3L;
-        CommentUpdateRequestDto requestDto=CommentUpdateRequestDto
+    public void updateComment() throws Exception {
+        String content = "api 테스트용 댓글 업데이트";
+        Long userId = 1L;
+        Long postId = 5L;
+
+        CommentUpdateRequestDto requestDto = CommentUpdateRequestDto
                 .builder()
                 .content(content)
                 .build();
-        URI uri= UriComponentsBuilder
-                .fromUriString("http://localhost:"+port)
-                .path("/api/v1/comments/posts/{postId}/users/{userId}").encode()
-                .build().expand(postId,userId).toUri();
-        System.out.println("uri = " + uri);
+
+        String url = "http://localhost:" + port + "/api/v1/comments";
+
         //when
         System.out.println(" ###UPDATE START");
-        HttpEntity<CommentUpdateRequestDto> requestEntity=new HttpEntity<>(requestDto);
-        ResponseEntity<CommentResponseDto> responseEntity=restTemplate.exchange(uri, HttpMethod.PUT,requestEntity,CommentResponseDto.class);
-        System.out.println(" ###UPDATE END");
-        System.out.println("responseEntity.toString() = " + responseEntity.toString());
-        if(responseEntity.getBody().getSuccess().equals(true)){
-            assertThat(responseEntity.getBody().getUserId()).isEqualTo(userId);
-            assertThat(responseEntity.getBody().getComment()).isEqualTo(content);
+        ResultActions actions = mvc.perform(put(url + "/posts/{postId}/users/{userId}", postId, userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(requestDto)));
 
-        }
+        System.out.println(" ###UPDATE END");
+
+        //then
+        actions.andExpect(status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.success").value("true"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.comment").value(content))
+
+                .andDo(MockMvcResultHandlers.print());
+
+        List<Comments> all = commentsRepository.findAll();
+        assertThat(all.get(10).getContent()).isEqualTo(content);
+
     }
 }
